65000492b9f2eb3d891ba2fc66f2d7f779065f24
murali <murali.pydi@gmail.com>
2025-11-12 21:51:03 +0530

    terraform aws roboshop

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01602722QJVZMCD67LWU"
+}
+
+variable "zone_name" {
+    default = "muraliitemp.online"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
